1c7a2b17c9f876f78981099c198384dfc9938a71
sehako <103739784+sehako@users.noreply.github.com>
2024-09-07 15:28:55 +0900

    Title: 행복한 수열의 개수, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240907/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int answer = 0;
+		int[] temp = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				temp[j] = arr[i][j];
+			}
+			
+			int cur = temp[0];
+			int cnt = 1;
+			for (int j = 1; j < n; j++) {
+				if (cur == temp[j]) {
+					if (++cnt == m) {
+						answer++;
+						break;
+					}
+				}
+				else {
+					cur = temp[j];
+					cnt = 1;
+				}
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				temp[j] = arr[j][i];
+			}
+			
+			int cur = temp[0];
+			int cnt = 1;
+			for (int j = 1; j < n; j++) {
+				if (cur == temp[j]) {
+					if (++cnt == m) {
+						answer++;
+						break;
+					}
+				}
+				else {
+					cur = temp[j];
+					cnt = 1;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
